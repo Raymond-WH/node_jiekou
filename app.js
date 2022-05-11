dcb228c70229788ff1c58790d8106d188f4d024e
水木年华 <2461981896@qq.com>
2022-05-11 20:02:59 +0800

    配置解析token中间件

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 // 导入并配置cors中间件
 const cors = require('cors');
 app.use(cors());
+
+
+
 // 配置解析表单数据的中间件,配置解析 `application/x-www-form-urlencoded`
 app.use(express.urlencoded({ ecxtended: false }));
 
@@ -23,6 +26,17 @@ app.use(function (req, res, next) {
   next()
 })
 
+// 一定要在路由配置之前解析token中间件
+// 导入配置文件
+const config = require('./config');
+
+// 解析token的中间件
+const {expressjwt:jwt} = require('express-jwt');
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证 
+app.use(jwt({
+  secret: config.jwtSecretKey, algorithms: ["HS256"],
+ }).unless({path:[/^\/api\//]}))
+
 // 导入并注册用户路由模块
 const userRouter = require('./router/user');
 // 现在用不了了
@@ -30,14 +44,21 @@ const userRouter = require('./router/user');
 // 安装npm i joi 
 const joi = require('joi')
 app.use('/api', userRouter)
+
 // 定义错误级别的中间件
-app.use((err, req, res, next) => { 
+app.use((err, req, res, next) => {
+  console.log(err);
   // 验证失败导致的错误
   if (err instanceof joi.ValidationError) return res.cc(err)
+
+  // 捕获身份认证失败的错误
+  if (err.name === 'UnauthorizedError') return res.cc('身份认证失败')
+
   // 未知的错误
   res.cc(err)
 })
+
 // 启动服务器
 app.listen(3000, () => {
   console.log('服务器启动成功，端口号为3000');
- })
+})
