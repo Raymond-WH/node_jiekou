7b330c01bb191667b037ec41755ff814b32dca38
水木年华 <2461981896@qq.com>
2022-05-13 07:31:48 +0800

    更新密码

--- router/userInfo.js
+++ router/userInfo.js
@@ -19,5 +19,12 @@ router.get('/userinfo', userInfoHandler.getUserInfo)
 // 更新用户基本信息
 router.post('/userinfo', expressJoi(update_userinfo_schema), userInfoHandler.updateUserInfo)
 
+
+// 导入密码验证规则
+const {update_password_schema } = require('../schema/user')
+// 更新密码
+router.post('/updatepwd', expressJoi(update_password_schema),userInfoHandler.updatePwd)
+
+
 // 向外共享路由对象
 module.exports = router
--- router_handler/userInfo.js
+++ router_handler/userInfo.js
@@ -1,7 +1,7 @@
 // 导入数据库模块
 const db = require('../db/index')
 
-
+const bcrypt = require('bcryptjs')
 
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
@@ -9,7 +9,7 @@ exports.getUserInfo = (req, res) => {
   // 注意：为了防止用户的密码泄露，需要排除 password 字段
   const sql = `select id, username, nickname,email, user_pic from ev_users where id=?`
   // 注意：req 对象上的 user 属性，是 Token 解析成功，express-jwt 中间件帮我们挂载上去的
-  console.log(req.auth)
+  // console.log(req.auth)
   db.query(sql, req.auth.id, (err, results) => {
     // 1. 执行 SQL 语句失败
     if (err) return res.cc(err)
@@ -29,7 +29,7 @@ exports.getUserInfo = (req, res) => {
 
 
 // 更新用户基本信息的处理函数
-exports.updateUserInfo = (req, res) => { 
+exports.updateUserInfo = (req, res) => {
 
   const sql = `update ev_users set ? where id=?`
   db.query(sql, [req.body, req.body.id], (err, results) => {
@@ -42,4 +42,57 @@ exports.updateUserInfo = (req, res) => {
     // 修改用户信息成功
     return res.cc('修改用户基本信息成功！', 0)
   })
+}
+
+// 更新密码的处理函数
+exports.updatePwd = (req, res) => {
+
+  // 定义根据 id 查询用户数据的 SQL 语句
+  const sql = `select * from ev_users where id=?`
+  // console.log(req);
+  // console.log(sql);
+  // 执行 SQL 语句查询用户是否存在
+  db.query(sql, req.auth.id, (err, results) => {
+
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+
+    // 检查指定 id 的用户是否存在
+    if (results.length !== 1) return res.cc('用户不存在！')
+
+    // TODO：判断提交的旧密码是否正确
+    // 判断提交的旧密码是否正确
+    // 在头部区域导入 bcryptjs 后，
+    // 即可使用 bcrypt.compareSync(提交的密码，数据库中的密码) 方法验证密码是否正确
+    // compareSync() 函数的返回值为布尔值，true 表示密码正确，false 表示密码错误
+    // const bcrypt = require('bcryptjs')
+
+    // 判断提交的旧密码是否正确
+    // console.log(results);
+    const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].password)
+    if (!compareResult) return res.cc('原密码错误！')
+    
+
+    // 对新密码进行brcypt加密更新到数据库中
+    // 定义更新用户密码的 SQL 语句
+    const sql = `update ev_users set password=? where id=?`
+
+    // 对新密码进行 bcrypt 加密处理
+    const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+
+    // 执行 SQL 语句，根据 id 更新用户的密码
+    db.query(sql, [newPwd, req.auth.id], (err, results) => {
+      // SQL 语句执行失败
+      if (err) return res.cc(err)
+
+      // SQL 语句执行成功，但是影响行数不等于 1
+      if (results.affectedRows !== 1) return res.cc('更新密码失败！')
+
+      // 更新密码成功
+      res.cc('更新密码成功！', 0)
+    })
+  })
+
+
+  
 }
