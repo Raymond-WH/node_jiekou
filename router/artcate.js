d25f2b7dee7267e6976cbe06da5afd67db9605ae
水木年华 <2461981896@qq.com>
2022-05-13 10:54:43 +0800

    新增文章分类功能

--- router/artcate.js
+++ router/artcate.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const artcate_handler = require('../router_handler/artcate')
+// 文章分类的列表数据
 router.get('/cates', artcate_handler.getArticleCates)
+
+// 新增文章分类的路由
+router.post('/addcates', artcate_handler.addArticleCates)
 
 module.exports = router
