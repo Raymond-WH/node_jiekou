fc6814914277065979b4e3cc4a35057512c88d21
水木年华 <2461981896@qq.com>
2022-05-13 14:04:40 +0800

    查询分类名称与别名是否被占用

--- router/artcate.js
+++ router/artcate.js
@@ -2,7 +2,7 @@ const expressJoi = require('@escook/express-joi')
 const express = require('express')
 const router = express.Router()
 const artcate_handler = require('../router_handler/artcate')
-const { delete_cate_schema, add_cate_schema } = require('../schema/artcate')
+const { delete_cate_schema, add_cate_schema, get_cate_schema, update_cate_schema } = require('../schema/artcate')
 // 文章分类的列表数据
 router.get('/cates', artcate_handler.getArticleCates)
 
@@ -14,5 +14,9 @@ router.post('/addcates', expressJoi(add_cate_schema),artcate_handler.addArticleC
 router.get('/deletecate/:id', expressJoi(delete_cate_schema),artcate_handler.deleteCateById)
 
 // 根据id获取文章分类路由
-// router.get('/cates/:id', artcate_handler.getArticleById)
+router.get('/cates/:id', expressJoi(get_cate_schema),artcate_handler.getArticleById)
+
+// 更新文章分类的路由
+router.post('/updatecate', expressJoi(update_cate_schema),artcate_handler.updateCateById)
+
 module.exports = router
